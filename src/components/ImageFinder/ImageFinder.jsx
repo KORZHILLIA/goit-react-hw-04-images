452338a7c521900adf7afd5dfd8773d58c60c396
KORZH ILLIA <kiev_drum2006@ukr.net>
2022-07-06 00:50:07 +0300

    Hooks

--- src/components/ImageFinder/ImageFinder.jsx
+++ src/components/ImageFinder/ImageFinder.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Oval } from 'react-loading-icons';
 import { fetchImages } from '../../shared/services/searchApi';
 import SearchBar from './SearchBar';
@@ -7,155 +7,151 @@ import Modal from '../../shared/components/Modal';
 import ImageGallery from './ImageGallery';
 import styles from './imageFinder.module.css';
 
-class ImageFinder extends Component {
-  state = {
-    query: '',
+const ImageFinder = () => {
+  const [state, setState] = useState({
     foundInfo: null,
     searchPageNumber: 1,
     loading: false,
+    error: null,
+    query: '',
+  });
+  const [modal, setModal] = useState({
     showModal: false,
     currentImg: null,
-    error: null,
-  };
+  });
 
-  componentDidUpdate(prevProps, prevState) {
-    const prevQuery = prevState.query;
-    const currentQuery = this.state.query;
-    const prevSearchPageNumber = prevState.searchPageNumber;
-    const { searchPageNumber: currentSearchPageNumber } = this.state;
-    if (prevQuery !== currentQuery) {
-      this.setState({ loading: true, searchPageNumber: 1, error: null });
-      this.getImages(currentQuery);
+  const { query, foundInfo, searchPageNumber, loading, error } = state;
+
+  const { showModal, currentImg } = modal;
+
+  const isFirstRender = useRef(true);
+
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
       return;
     }
-    if (prevSearchPageNumber !== currentSearchPageNumber) {
-      this.setState({
-        loading: true,
-        searchPageNumber: currentSearchPageNumber,
-        error: null,
-      });
-      this.getImages(currentQuery, currentSearchPageNumber);
-    }
-  }
 
-  submitHandler = query => {
-    this.setState({ query });
-  };
-
-  async getImages(query, page = 1) {
-    try {
-      const newFoundInfo = await fetchImages(query, page);
-      if (!newFoundInfo.hits.length) {
-        throw new Error('Nothing found, please try something else');
-      }
-      if (page === 1) {
-        this.setState({
-          foundInfo: newFoundInfo,
-          loading: false,
+    async function getImages(query, page = 1) {
+      try {
+        const newFoundInfo = await fetchImages(query, page);
+        if (!newFoundInfo.hits.length) {
+          throw new Error('Nothing found, please try something else');
+        }
+        if (page === 1) {
+          setState(prevState => ({
+            ...prevState,
+            foundInfo: newFoundInfo,
+            loading: false,
+          }));
+          return;
+        }
+        const { hits: newHits } = newFoundInfo;
+        setState(prevState => {
+          const { foundInfo } = prevState;
+          const { hits } = foundInfo;
+          const updatedHits = [...hits, ...newHits];
+          return {
+            ...prevState,
+            foundInfo: { ...foundInfo, hits: updatedHits },
+            loading: false,
+          };
         });
-        return;
-      }
-      const { hits: newHits } = newFoundInfo;
-      this.setState(prevState => {
-        const { foundInfo } = prevState;
-        const { hits } = foundInfo;
-        const updatedHits = [...hits, ...newHits];
-        return {
-          foundInfo: { ...foundInfo, hits: updatedHits },
+      } catch (error) {
+        setState(prevState => ({
+          ...prevState,
+          error: error.message,
+          foundInfo: null,
           loading: false,
-        };
-      });
-    } catch (error) {
-      this.setState({ error: error.message, foundInfo: null, loading: false });
+        }));
+      }
     }
-  }
 
-  loadMoreClickHandler = () => {
-    this.setState(({ searchPageNumber }) => ({
-      searchPageNumber: searchPageNumber + 1,
-    }));
-  };
-
-  setCurrentImg = img => {
-    this.setState({ currentImg: img, showModal: true });
-  };
-
-  closeModal = () => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
-  };
-
-  render() {
-    const { submitHandler, loadMoreClickHandler, setCurrentImg, closeModal } =
-      this;
-    const {
-      foundInfo,
-      loading,
+    setState(prevState => ({
+      ...prevState,
+      loading: true,
       searchPageNumber,
-      showModal,
-      currentImg,
-      error,
-    } = this.state;
+      error: null,
+    }));
 
-    return (
-      <>
-        <SearchBar onSubmit={submitHandler} />
-        {!foundInfo && error && <p className={styles.errorMessage}>{error}</p>}
-        {!foundInfo && loading ? (
-          <Oval
-            className={styles.spinner}
-            stroke="#3f51b8"
-            fill="#3f51b5"
-            strokeOpacity={0.825}
-            speed={2.0}
-          />
-        ) : null}
+    getImages(query, searchPageNumber);
+  }, [searchPageNumber, query]);
 
-        {foundInfo && error && <p className={styles.errorMessage}>{error}</p>}
-        {showModal && (
-          <Modal onClose={closeModal}>
-            {
-              <img
-                className={styles.modalImg}
-                src={currentImg.img}
-                alt={currentImg.alt}
-              />
-            }
-          </Modal>
-        )}
-        {loading && searchPageNumber === 1 && (
-          <Oval
-            className={styles.spinner}
-            stroke="#3f51b8"
-            fill="#3f51b5"
-            strokeOpacity={0.825}
-            speed={2.0}
-          />
-        )}
-        {foundInfo && !error && (
-          <ImageGallery
-            hits={foundInfo.hits}
-            currentImgChanger={setCurrentImg}
-          />
-        )}
-        {loading && searchPageNumber > 1 && (
-          <Oval
-            className={styles.spinner}
-            stroke="#3f51b8"
-            fill="#3f51b5"
-            strokeOpacity={0.825}
-            speed={2.0}
-          />
-        )}
-        {foundInfo &&
-        !error &&
-        Math.ceil(foundInfo.total / foundInfo.hits.length) > 1 ? (
-          <Button onClick={loadMoreClickHandler} />
-        ) : (
-          ''
-        )}
-      </>
-    );
+  function submitHandler(query) {
+    setState(prevState => ({ ...prevState, query, searchPageNumber: 1 }));
   }
-}
+
+  function loadMoreClickHandler() {
+    setState(prevState => ({
+      ...prevState,
+      searchPageNumber: prevState.searchPageNumber + 1,
+    }));
+  }
+
+  function setCurrentImg(img) {
+    setModal({ currentImg: img, showModal: true });
+  }
+
+  function closeModal() {
+    setModal(prevState => ({ ...prevState, showModal: false }));
+  }
+
+  return (
+    <>
+      <SearchBar onSubmit={submitHandler} />
+      {!foundInfo && error && <p className={styles.errorMessage}>{error}</p>}
+      {!foundInfo && loading ? (
+        <Oval
+          className={styles.spinner}
+          stroke="#3f51b8"
+          fill="#3f51b5"
+          strokeOpacity={0.825}
+          speed={2.0}
+        />
+      ) : null}
+
+      {foundInfo && error && <p className={styles.errorMessage}>{error}</p>}
+      {showModal && (
+        <Modal onClose={closeModal}>
+          {
+            <img
+              className={styles.modalImg}
+              src={currentImg.img}
+              alt={currentImg.alt}
+            />
+          }
+        </Modal>
+      )}
+      {loading && searchPageNumber === 1 && (
+        <Oval
+          className={styles.spinner}
+          stroke="#3f51b8"
+          fill="#3f51b5"
+          strokeOpacity={0.825}
+          speed={2.0}
+        />
+      )}
+      {foundInfo && !error && (
+        <ImageGallery hits={foundInfo.hits} currentImgChanger={setCurrentImg} />
+      )}
+      {loading && searchPageNumber > 1 && (
+        <Oval
+          className={styles.spinner}
+          stroke="#3f51b8"
+          fill="#3f51b5"
+          strokeOpacity={0.825}
+          speed={2.0}
+        />
+      )}
+      {foundInfo &&
+      !error &&
+      Math.ceil(foundInfo.total / foundInfo.hits.length) > 1 ? (
+        <Button onClick={loadMoreClickHandler} />
+      ) : (
+        ''
+      )}
+    </>
+  );
+};
 
 export default ImageFinder;
--- src/shared/components/Modal/Modal.jsx
+++ src/shared/components/Modal/Modal.jsx
@@ -1,41 +1,39 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { createPortal } from 'react-dom';
 import styles from './modal.module.css';
 
 const modalRoot = document.querySelector('#modal-root');
 
-class Modal extends Component {
-  static defaultProps = {
-    onClose: () => {},
-  };
+const Modal = ({ onClose, children }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', closeModal);
 
-  componentDidMount() {
-    window.addEventListener('keydown', this.closeModal);
-  }
+    return () => {
+      window.removeEventListener('keydown', closeModal);
+    };
+  });
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.closeModal);
-  }
-
-  closeModal = ({ code, target, currentTarget }) => {
+  function closeModal({ code, target, currentTarget }) {
     if (code === 'Escape' || target === currentTarget) {
-      this.props.onClose();
+      onClose();
     }
-  };
-  render() {
-    const { closeModal } = this;
-    return createPortal(
-      <div className={styles.overlay} onClick={closeModal}>
-        <div className={styles.modal}>{this.props.children}</div>
-      </div>,
-      modalRoot
-    );
   }
-}
+
+  return createPortal(
+    <div className={styles.overlay} onClick={closeModal}>
+      <div className={styles.modal}>{children}</div>
+    </div>,
+    modalRoot
+  );
+};
+
+Modal.defaultProps = {
+  onClose: () => {},
+};
 
 Modal.propTypes = {
-  onClose: PropTypes.func.isRequired,
+  onClose: PropTypes.func,
 };
 
 export default Modal;
